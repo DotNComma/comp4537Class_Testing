6f023d86b7f9ce9d0c130cb7dd9d815cd2db6e28
Carl Manansala <cmanansala3@my.bcit.ca>
2026-02-03 10:18:52 -0800

    changed back

--- test.js
+++ test.js
@@ -3,13 +3,5 @@ const port = process.env.PORT || 8000;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html", "Access-Control-Allow-Origin": "*"})
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://apiz.ca/readystatechange", true);
-    xhttp.send();
-    xhttp.onreadystatechange = function () {
-        console.log("hello")
-    }
     res.end('Hello <b>World</b>')
 }).listen(port);
-
-console.log('Server is running and listening to 8888')
