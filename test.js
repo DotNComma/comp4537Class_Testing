e93e47b0b606449fe1956099600734193def01ca
Carl Manansala <cmanansala3@my.bcit.ca>
2026-02-03 10:12:31 -0800

    moved the ajx call to the server

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
 const http = require('http');
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html", "Access-Control-Allow-Origin": "*"})
     res.end('Hello <b>World</b>')
 }).listen(port);
+
+const xhttp = new XMLHttpRequest();
+xhttp.open("GET", "http://apiz.ca/readystatechange", true);
+xhttp.send();
+xhttp.onreadystatechange = function () {
+    console.log("hello")
+}
 
 console.log('Server is running and listening to 8888')
