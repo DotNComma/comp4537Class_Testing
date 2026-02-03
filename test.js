72992e7aed2d01acedbd803626a73c11399d1f06
Carl Manansala <cmanansala3@my.bcit.ca>
2026-02-03 09:46:45 -0800

    Added the server's port

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const http = require('http');
+const port = process.env.PORT || 8000
 
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html", "Access-Control-Allow-Origin": "*"})
     res.end('Hello <b>World</b>')
-}).listen(8888);
+}).listen(port);
 
 console.log('Server is running and listening to 8888')
