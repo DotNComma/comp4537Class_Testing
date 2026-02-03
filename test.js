ca952496b1a51907a0b1a0dc2d87cfd8d50f16b1
Carl Manansala <cmanansala3@my.bcit.ca>
2026-02-03 10:14:38 -0800

    ajax in server

--- test.js
+++ test.js
@@ -3,14 +3,13 @@ const port = process.env.PORT || 8000;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html", "Access-Control-Allow-Origin": "*"})
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://apiz.ca/readystatechange", true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        console.log("hello")
+    }
     res.end('Hello <b>World</b>')
 }).listen(port);
-
-const xhttp = new XMLHttpRequest();
-xhttp.open("GET", "http://apiz.ca/readystatechange", true);
-xhttp.send();
-xhttp.onreadystatechange = function () {
-    console.log("hello")
-}
 
 console.log('Server is running and listening to 8888')
